0b55a1362d7844917e4e0d6577466fe0ff49148a
Yeasin <yeasin30112001@gmail.com>
2021-02-12 17:35:17 -0600

    learn typescript type string boolean and number type

--- first.ts
+++ first.ts
@@ -1,3 +1,16 @@
-let myAmount = 50;
-myAmount = 150;
-myAmount = 10;
+// let age = 50;
+// let isFamous = true;
+// let text = "hello";
+
+let age: number = 50;
+let isFamous: boolean = false;
+let text: string = 'I love my country';
+
+
+// const famous: boolean; // it will show an error because a const variable can't be written without assigning a value;
+
+const famous: boolean = false;
+// famous = true // it will show an error because constant variable can't be re-assign
+
+
+age = 100;
